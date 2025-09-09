6cfde826e428180c3d90533993a25ad6c0baf517
Ruan Torres <oxyruan@gmail.com>
2025-09-09 16:54:54 -0300

    Atividade Validação

---
+++ _js/script.js
@@ -0,0 +1,17 @@
+function verificarDados() {
+    const nome = document.getElementById('nome-user').value;
+    const cpf = document.getElementById('cpf-user').value;
+    const regexNome = /^[A-Za-zÀ-ÖØ-öø-ÿ'\- ]+$/;
+    const regexCpf = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/;
+;
+
+    if (!regexNome.test(nome)) {
+        alert('Coloque seu nome corretamente!');
+        return false;
+    }
+
+    if (!regexCpf.test(cpf)) {
+        alert('CPF inválido! Por-favor, digite seu CPF da forma correta.');
+    }
+    return true;
+}
